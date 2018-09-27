b77ab0a6ba0821fff932b2e21d7a5efcea34dddc
HP <HP@HP-PC>
2018-09-28 00:09:03 +0100

    Map Functions

---
+++ java8-Tests/src/ma/tests/lambdas/FilterMapTests.java
@@ -0,0 +1,34 @@
+package ma.tests.lambdas;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FilterMapTests {
+
+	public static void main(String[] args) {
+		
+		
+		Map<Integer, String> map = new HashMap<>();
+	    map.put(1, "linode.com");
+	    map.put(2, "heroku.com");
+		
+		//Map -> Stream -> Filter -> String
+		String result = map.entrySet().stream()
+			.filter(x -> "something".equals(x.getValue()))
+			.map(x->x.getValue())
+			.collect(Collectors.joining());
+
+		//Map -> Stream -> Filter -> MAP
+		Map<Integer, String> collect = map.entrySet().stream()
+			.filter(x -> x.getKey() == 2)
+			.collect(Collectors.toMap(x -> x.getKey(), x -> x.getValue()));
+			
+		// or like this
+		Map<Integer, String>  collect1 = map.entrySet().stream()
+			.filter(x -> x.getKey() == 3)
+			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+	}
+
+}
--- java8-Tests/src/ma/tests/lambdas/MapTest.java
+++ java8-Tests/src/ma/tests/lambdas/MapTest.java
@@ -1,7 +1,10 @@
 package ma.tests.lambdas;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import ma.beans.lambdas.Developer;
@@ -17,5 +20,30 @@ public class MapTest {
 		                                                          return dev;}).collect(Collectors.toList());
 		
 		System.out.println(devlopers);
+		
+		Map<String, Integer> unsortMap = new HashMap<>();
+        unsortMap.put("z", 10);
+        unsortMap.put("b", 5);
+        unsortMap.put("a", 6);
+        unsortMap.put("c", 20);
+        unsortMap.put("d", 1);
+        unsortMap.put("e", 7);
+        unsortMap.put("y", 8);
+        unsortMap.put("n", 99);
+        unsortMap.put("g", 50);
+        unsortMap.put("m", 2);
+        unsortMap.put("f", 9);
+		 // sort by keys, a,b,c..., and return a new LinkedHashMap
+        // toMap() will returns HashMap by default, we need LinkedHashMap to keep the order.
+        //
+        
+        //************** (oldValue, newValue) -> oldValue if not specified the code will throw an error if there is a duplicate key
+        Map<String, Integer> result = unsortMap.entrySet().stream()
+        .sorted(Map.Entry.comparingByKey())
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+        
+        System.out.println("Sorted...");
+        System.out.println(result);
 	}
 }
