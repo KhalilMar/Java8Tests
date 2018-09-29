0e7a42c9c52c74b9488d499735a47b24cfa00851
moun3imy <mounaim.zaryouhi@gmail.com>
2018-09-29 15:52:52 +0100

    added attribute to Developer class

--- java8-Tests/src/ma/beans/lambdas/Developer.java
+++ java8-Tests/src/ma/beans/lambdas/Developer.java
@@ -3,6 +3,15 @@ package ma.beans.lambdas;
 public class Developer {
 
 	private String name;
+	private String code;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
 
 	public Developer(String name) {
 		super();
