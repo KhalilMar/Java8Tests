1f3e9bad9dbd378a98dc3e1d310ffce62f878b68
lenovo <lenovo@DESKTOP-J5IM1N2>
2018-12-18 19:23:00 +0100

    Parallel Stream

---
+++ java8-Tests/src/ma/tests/lambdas/ParallelStream.java
@@ -0,0 +1,32 @@
+package ma.tests.lambdas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ParallelStream {
+
+	public static void main(String[] args) {
+		
+		List<String> lst  = new ArrayList<String>();
+		
+		for(int i=0; i<10000000;i++) {
+			
+		 lst.add("Mounaim"+i);
+		}
+		long start1 = System.nanoTime();
+		Optional<Integer> o = lst.stream().map(s-> s.length()).reduce((a,b) -> a+b);
+		long end1 = System.nanoTime();
+
+		System.out.println(" ---------- " + ((end1-start1)/1000000));
+
+		long start2 = System.nanoTime();
+
+		lst.parallelStream().map(s-> s.length()).reduce((a,b) -> a+b);
+		long end2 = System.nanoTime();
+		
+		System.out.println(" ---------- " + ((end2-start2)/1000000));
+
+	}
+
+}
