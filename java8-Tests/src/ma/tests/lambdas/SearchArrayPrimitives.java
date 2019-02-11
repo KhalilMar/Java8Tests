a0c5699c47073f3ce7d1429f79f0922a74ad337e
lenovo <lenovo@DESKTOP-J5IM1N2>
2019-02-11 16:45:10 +0100

    Format

--- java8-Tests/src/ma/tests/lambdas/SearchArrayPrimitives.java
+++ java8-Tests/src/ma/tests/lambdas/SearchArrayPrimitives.java
@@ -6,27 +6,27 @@ import java.util.stream.LongStream;
 public class SearchArrayPrimitives {
 
 	public static void main(String[] args) {
-		
-		 int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-	        //Java 8
-	        boolean result = IntStream.of(number).anyMatch(x -> x == 4);
+		int[] number = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-	        if (result) {
-	            System.out.println("Hello 4");
-	        } else {
-	            System.out.println("Where is number 4?");
-	        }
+		// Java 8
+		boolean result = IntStream.of(number).anyMatch(x -> x == 4);
 
-	        long[] lNumber = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		if (result) {
+			System.out.println("Hello 4");
+		} else {
+			System.out.println("Where is number 4?");
+		}
 
-	        boolean result2 = LongStream.of(lNumber).anyMatch(x -> x == 10);
+		long[] lNumber = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-	        if (result2) {
-	            System.out.println("Hello 10");
-	        } else {
-	            System.out.println("Where is number 10?");
-	        }
+		boolean result2 = LongStream.of(lNumber).anyMatch(x -> x == 10);
+
+		if (result2) {
+			System.out.println("Hello 10");
+		} else {
+			System.out.println("Where is number 10?");
+		}
 
 	}
 
